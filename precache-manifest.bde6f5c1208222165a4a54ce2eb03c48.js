cea4159f84b2ac049a6e2dd7c41ac781a4786ad9
unknown <ivan.nikic@gmail.com>
2020-09-18 20:02:41 +0200

    Updates

---
+++ precache-manifest.bde6f5c1208222165a4a54ce2eb03c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc316249b0ce986e466792386ef5b29",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "c62bc0ee2024603ad87d",
+    "url": "/pathfinder/static/css/main.4453a7fc.chunk.css"
+  },
+  {
+    "revision": "7fb39996b7e6bac71fb4",
+    "url": "/pathfinder/static/js/2.39f4d220.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pathfinder/static/js/2.39f4d220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62bc0ee2024603ad87d",
+    "url": "/pathfinder/static/js/main.25f2abae.chunk.js"
+  },
+  {
+    "revision": "acf8cfc3d775afb9765b",
+    "url": "/pathfinder/static/js/runtime-main.26183e5d.js"
+  }
+]);
